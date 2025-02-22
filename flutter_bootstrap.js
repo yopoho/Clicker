80851f04483105f03cda02f67166ed2e8c1bd01c
yopopohu21 <yopopohu21@gmail.com>
2025-02-22 13:15:46 +0100

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3982582425"
+    serviceWorkerVersion: "1599322308"
   }
 });
