bc5e0bac4c73a6352932f43c769c4f208a82bd7e
alexa <alexander.a.seregin@gmail.com>
2025-06-14 16:09:56 +0200

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"109150893958777c8f2215f6cfd3e89e984e8d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "743189147"
+    serviceWorkerVersion: "1250198411"
   }
 });
